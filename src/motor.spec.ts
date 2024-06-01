8513652e8de2a6f38a561c2eda860a0c7ceb338b
Oscar <trabajos1379@gmail.com>
2024-06-01 12:26:35 +0200

    testing de comprobarNumero evaluando todos los estados/puntuactiones

--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -7,6 +7,47 @@ describe('comprobarNumero', () => {
     //Arrange
     const estadoEsperado: Estado = 'WIN';
     const puntuacion: number = 7.5;
+    //vi.spyOn(puntuacion, 'comprobarNumero', 'get').mockReturnValue(7.5);
+    //Act
+    const win = comprobarNumero(puntuacion);
+    //Assert
+    expect(win).toBe(estadoEsperado);
+  });
+
+  it('puntuacion > 7.5 => estado  es GAMEOVER', () => {
+    //Arrange
+    const estadoEsperado: Estado = 'GAMEOVER';
+    const puntuacion: number = 10;
+    //Act
+    const win = comprobarNumero(puntuacion);
+    //Assert
+    expect(win).toBe(estadoEsperado);
+  });
+
+  it('puntuacion <= 4 estado  es CONSERVADOR', () => {
+    //Arrange
+    const estadoEsperado: Estado = 'CONSERVADOR';
+    const puntuacion: number = 3;
+    //Act
+    const win = comprobarNumero(puntuacion);
+    //Assert
+    expect(win).toBe(estadoEsperado);
+  });
+
+  it('puntuacion > 4 && puntuacion < 6  es CAGADO', () => {
+    //Arrange
+    const estadoEsperado: Estado = 'CAGADO';
+    const puntuacion: number = 5;
+    //Act
+    const win = comprobarNumero(puntuacion);
+    //Assert
+    expect(win).toBe(estadoEsperado);
+  });
+
+  it('puntuacion >= 6 && puntuacion < 7.5  es CASI', () => {
+    //Arrange
+    const estadoEsperado: Estado = 'CASI';
+    const puntuacion: number = 6;
     //Act
     const win = comprobarNumero(puntuacion);
     //Assert
